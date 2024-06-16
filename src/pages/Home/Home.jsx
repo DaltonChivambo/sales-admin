368c21a0e9eea95321c2ff629bc214941f38aae5
DaltonChivambo <daltonchivambo@gmail.com>
2024-06-16 09:41:15 +0200

    add chart userData to Dashboard

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './Home.css'
 import FeaturedInfo from '../../components/FeaturedInfo/FeaturedInfo'
+import Chart from '../../components/Chart/Chart'
+import {userData} from "../../dummyData"
 
 const Home = () => {
   return (
     <div className='home'>
       <FeaturedInfo/>
+      <Chart title="User Analytics" data={userData} grid dataKey="Active User"/>
     </div>
   )
 }
